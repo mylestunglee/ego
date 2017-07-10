797abe122267f0a99a350cc8bc873645dbe5916f
Myles Lee <mtl115@ic.ac.uk>
2017-07-10 13:16:55 +0100

    implemented error handling for file arguments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
+#include <stdexcept>
+#include "main.hpp"
 #include "ego.hpp"
-#include "surrogate.hpp"
 #include "evaluator.hpp"
 #include "transferrer.hpp"
 #include "functions.hpp"
@@ -8,70 +9,89 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	if (argc == 5) {
-		// Using knowledge transfer
-		string filename_results_old(argv[1]);
-		string filename_script(argv[2]);
-		string filename_config(argv[3]);
-		string filename_output(argv[4]);
-
-		auto config = read(filename_config);
-
-		size_t max_evaluations = stoi(config[0][0]);
-		size_t max_trials = stoi(config[1][0]);
-		double convergence_threshold = stof(config[2][0]);
-		bool is_discrete = config[3][0].compare("true") == 0;
-		size_t constraints = stoul(config[4][0]);
-		size_t costs = stoul(config[5][0]);
-		boundaries_t boundaries = read_boundaries(config[6], config[7]);
-		double sig_level = stof(config[8][0]);
-
-		Evaluator evaluator(filename_script);
-		Transferrer transferrer(
-			filename_results_old, evaluator, max_evaluations,
-			max_trials, convergence_threshold, sig_level, boundaries,
-			is_discrete, constraints, costs);
-		transferrer.run();
-		evaluator.save(filename_output);
-		return 0;
-	} else if (argc == 4) {
-		// Not using knowledge transfer
-		string script(argv[1]);
-		string filename_config(argv[2]);
-		string filename_output(argv[3]);
-
-		auto config = read(filename_config);
-
-		size_t max_evaluations = stoul(config[0][0]);
-		size_t max_trials = stoul(config[1][0]);
-		double convergence_threshold = stof(config[2][0]);
-		// TODO: handle neither true or false exception
-		bool is_discrete = config[3][0].compare("true") == 0;
-		size_t constraints = stoul(config[4][0]);
-		size_t costs = stoul(config[5][0]);
-		boundaries_t boundaries = read_boundaries(config[6], config[7]);
-
-		Evaluator evaluator(script);
-
-		EGO ego(evaluator, boundaries, {}, max_evaluations, max_trials,
-			convergence_threshold, is_discrete, constraints, costs);
-
-		// Heuristic sample size = 5 * dim
-		ego.sample_latin(10);
-		// Randomness can offset optimiser negatively
-		//ego.sample_uniform(10);
-		ego.run();
-		evaluator.save(filename_output);
-
-
+	if (argc != 4 && argc != 5) {
+		print_help();
 		return 0;
 	}
 
-	cout << "Usage:" << endl;
-	cout << "Without knowledge transfer: ego script config log" << endl;
-	cout << "Script calls the fitness function; config contains hyperparameters for this program; log is the output that stores calls to the script." << endl;
-	cout << "With knowledge transfer: ego results script config log" << endl;
-	cout << "Results is the log of the previous (optimised) design." << endl;
+	bool is_knowledge_transfer = argc == 5;
+
+	string filename_script(argv[1]);
+	string filename_config(argv[2]);
+	string filename_output(argv[3]);
+
+	auto config = read(filename_config);
+
+	size_t max_evaluations, max_trials, constraints, costs;
+	double convergence_threshold, sig_level = 1.0;
+	bool is_discrete;
+	boundaries_t boundaries;
+
+	try {
+		max_evaluations = stoi(config.at(0).at(0));
+		max_trials = stoi(config.at(1).at(0));
+		convergence_threshold = stof(config.at(2).at(0));
+		string is_discrete_string = config.at(3).at(0);
+		if (is_discrete_string.compare("true") != 0 &&
+				is_discrete_string.compare("false") != 0) {
+			throw invalid_argument("is_discrete is not a boolean");
+		}
+		is_discrete = config.at(3).at(0).compare("true") == 0;
+		constraints = stoul(config.at(4).at(0));
+		costs = stoul(config.at(5).at(0));
+		boundaries = read_boundaries(config.at(6), config.at(7));
+		if (is_knowledge_transfer) {
+			stof(config.at(8).at(0));
+		}
+	} catch (const invalid_argument& ia) {
+		cerr << "Invalid value in configuration file: " << ia.what() << endl;
+		return 0;
+	} catch (const out_of_range& oor) {
+		cerr << "Insufficent values in configuration file: " << oor.what() << endl;
+		return 0;
+	}
+
+	Evaluator evaluator(filename_script);
+
+	if (is_knowledge_transfer) {
+		string filename_results_old(argv[4]);
+
+		Transferrer transferrer(
+			filename_results_old,
+			evaluator,
+			max_evaluations,
+			max_trials,
+			convergence_threshold,
+			sig_level,
+			boundaries,
+			is_discrete,
+			constraints,
+			costs);
+		transferrer.run();
+	} else {
+		EGO ego(
+			evaluator,
+			boundaries,
+			{},
+			max_evaluations,
+			max_trials,
+			convergence_threshold,
+			is_discrete,
+			constraints,
+			costs);
+		// Heuristic sample size = 5 * dim
+		ego.sample_latin(5 * boundaries.size());
+		// Randomness can affect optimiser negatively
+		//ego.sample_uniform(10);
+		ego.run();
+	}
+
+	evaluator.save(filename_output);
 
 	return 0;
+}
+
+void print_help() {
+	cout << "Usage: ego script config log [results]" << endl;
+	cout << "\tTo use knowledge transfer, specify some results in CSV format." << endl;
 }
---
+++ main.hpp
@@ -0,0 +1,2 @@
+int main(int argc, char* argv[]);
+void print_help();
